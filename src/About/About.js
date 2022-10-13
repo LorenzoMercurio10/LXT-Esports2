71e18ddbcc65c6fe357df43025c8fd0f8fe82055
LorenzoMercurio10 <lorenzo.mercurio@studenti.polito.it>
2022-10-13 15:04:23 +0200

    fixed pt.2

--- src/About/About.js
+++ src/About/About.js
@@ -10,7 +10,7 @@ function About() {
                     <p>Ask Advisory, società benefit, ha lanciato il suo primo progetto con finalità di “beneficio comune”, costituendo il proprio team <span class="lxt-esports">LXT ESPORTS</span>.</p>
                     <p className="about-history">In collaborazione con Lexant, società benefit tra avvocati, LXT Esports, sarà coinvolto nella competizione di due titoli, <span class="fifa-23">FIFA 23</span> e <span class="valorant">Valorant</span>.</p> 
                     <p className="about-history">
-                    Il nuovo team metterà a disposizione del mercato i propri contenuti formativi e promuoverà iniziative di informazione e sensibilizzazione sui rischi connessi, come quelli del cyber bullismo, del match fixing, della privacy e della non corretta alimentazione.</p>    
+                    Il nuovo team metterà a disposizione del mercato i propri contenuti formativi e promuoverà iniziative di informazione e sensibilizzazione sui rischi connessi, come quelli del cyber bullismo, del match fixing, della ludopatia, della privacy e della non corretta alimentazione.</p>    
                 </div>
             </div>
         </section>
