3b2bedf27954f8e46e07d7a6b01ca93207a9fbd3
LorenzoMercurio10 <lorenzo.mercurio@studenti.polito.it>
2023-02-15 16:04:36 +0100

    link changed

--- src/Teams/Academy.js
+++ src/Teams/Academy.js
@@ -10,7 +10,7 @@ function About() {
                         <h1 className="academy-header" target="_blank">FORMAZIONE</h1>
                         <p>LXT Esports è partner didattico di <span className="name-ask">Academy ASK</span> nello studio e nella erogazione dei corse della nuova offerta formativa dedicata al mondo degli Esports.</p>
                         <p>Grazie all’esperienza maturata nel settore dei gamers e dai professionisti di LXT Esports, la formazione di <span className="name-ask">Academy ASK</span> si arricchisce di nuove metodologie di apprendimento da oggi più che mai integrate e con le specifiche competenze ed abilità proprie del mondo del gaming.</p>   
-                        <p><a href="https://www.askadvisory.it/uploads/generic_file/brochure_esport_new_version.pdf" className="link-ask-info" target="_blank">Corso di formazione Gaming & Esports</a></p>
+                        <p><a href="https://www.askadvisory.it/it/pages/96-corsi-di-formazione" className="link-ask-info" target="_blank">Corso di formazione Gaming & Esports</a></p>
                         <p><a href="https://www.askadvisory.it/uploads/generic_file/catalogo_esports_2023_new_version-_8_02_23.pdf" className="link-ask-info" target="_blank">Catalogo Esports 2023</a></p>
                         <p><a href="https://www.askadvisory.it/corsi-formazione-ask/" className="link-ask-info" target="_blank">Academy ASK</a></p>
                     </div>
