ae9209a2403d39690047bd49472874da562dfec6
LorenzoMercurio10 <lorenzo.mercurio@studenti.polito.it>
2023-02-17 16:38:34 +0100

    staff added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
       <Benefit />
       <Blog />
       <Teams />
+      <Staff />
       <Partner />
       <Academy />
       <Contact />
--- src/Teams/Staff.js
+++ src/Teams/Staff.js
@@ -1,5 +1,4 @@
 import './Staff.css'
-import Nav from 'react-bootstrap/Nav';
 
 function Staff() {
     return (
@@ -11,93 +10,63 @@ function Staff() {
                 <article className='staffer'>                    
                     <div className="row justify-content-center fifa-card">
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
-                            <img className="staff1" src="img/mileto.png" alt="player-1" width="275" height="275" />
-                            <div className="nick-player1-fifa">
-                                <span className="name-player1-fifa">Andrea Mileto</span>
-                                <span className="role-player1-fifa">Team Manager</span>
-                                <div className="socials-player1-fifa">
-                                    <Nav.Link href="https://ww.instagram.com/crazy_fat_gamer/" target="_blank" className="player-instagram">
-                                        <img src="img/ig.png" alt="instagram" height="23" width="25"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://titter.com/CRAZYFATGAMER" target="_blank" className="twitter">
-                                        <img src="img/twitter.png" alt="twitter" height="23" width="27"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://ww.twitch.tv/crazyfatgamer" target="_blank" className="twitch">
-                                        <img src="img/twitch.png" alt="twitch" height="23" width="23"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://ww.tiktok.com/@crazyfatgamer" target="_blank" className="tiktok">
-                                        <img src="img/tiktok.png" alt="tiktok" height="23" width="23"></img>
-                                    </Nav.Link>
-                                </div>
+                            <div className="nick-player1-fifa justify-content-center">
+                                <span className="name-player1-fifa">Roberto Giancarlo Daverio</span>
+                                <span className="role-player1-fifa">Presidente</span>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
-                            <img className="staff2" src="img/vittorio.png" alt="player-2" width="275" height="275"></img>
                             <div className="nick-player1-fifa">
-                                <span className="name-player1-fifa">Vittorio Cicatiello</span>
+                                <a className="link-staff" href="https://www.linkedin.com/in/vittorio-cicatiello-a05077180/" target="_blank">
+                                    <span className="nick-staff">Vittorio Cicatiello</span>
+                                </a>
                                 <span className="role-player1-fifa">Team Manager</span>
-                                <div className="socials-player1-fifa">
-                                    <Nav.Link href="https://wwwinsagram.com/storari30/" target="_blank" className="player-instagram">
-                                        <img src="img/ig.png" alt="instagram" height="23" width="25"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://twiter.com/Storari30" target="_blank" className="player-twitch">
-                                        <img src="img/twitter.png" alt="twitch" height="23" width="27"></img>
-                                    </Nav.Link>
-                                </div>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
-                            <img className="staff2" src="img/cipolla.png" alt="player-2" width="275" height="275"></img>
                             <div className="nick-player1-fifa">
-                                <span className="name-player1-fifa">Vincenzo Cipolla</span>
-                                <span className="role-player1-fifa">Fifa Coach</span>
-                                <div className="socials-player1-fifa">
-                                    <Nav.Link href="https://www.instagram.com/_vincenzocipolla_/?igshid=1kfk18j413lll" target="_blank" className="player-instagram">
-                                        <img src="img/ig.png" alt="instagram" height="23" width="25"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://twitter.com/VincenzoCFifa" target="_blank" className="tiktok">
-                                        <img src="img/twitter.png" alt="tiktok" height="23" width="27"></img>
-                                    </Nav.Link>
-                                </div>
+                                <span className="name-player1-fifa">Simona Cardillo</span>
+                                <span className="role-player1-fifa">Responsabile Impatto</span>
                             </div>
                         </div>
                     </div>
                     <div className="row justify-content-center fifa-card">
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
-                            <img className="staff1" src="img/zhevor.png" alt="player-1" width="275" height="275" />
                             <div className="nick-player1-fifa">
-                                <span className="name-player1-fifa">Zhevorr</span>
+                                <span className="name-player1-fifa">Vincenzo Cipolla</span>
+                                <span className="role-player1-fifa">Fifa Coach</span>
+                            </div>
+                        </div>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
+                            <div className="nick-player1-fifa">
+                                <span className="name-player1-fifa">Riccardo Rugoletti</span>
                                 <span className="role-player1-fifa">Valorant Coach</span>
-                                <div className="socials-player1-fifa">
-                                    <Nav.Link href="https://www.instagram.com/zhevor/" target="_blank" className="player-instagram">
-                                        <img src="img/ig.png" alt="instagram" height="23" width="25"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://twitter.com/zhevor" target="_blank" className="twitter">
-                                        <img src="img/twitter.png" alt="twitter" height="23" width="27"></img>
-                                    </Nav.Link>
-                                    <Nav.Link href="https://www.twitch.tv/zhevor/" target="_blank" className="twitch">
-                                        <img src="img/twitch.png" alt="twitch" height="23" width="23"></img>
-                                    </Nav.Link>
-                                </div>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
-                            <img className="staff2" src="img/omino_stilizzato.png" alt="player-2" width="275" height="275"></img>
                             <div className="nick-player1-fifa">
-                                <span className="name-player1-fifa">Psychologist</span>
-                                <span className="role-player1-fifa">Psychologist</span>
-                                <div className="socials-player1-fifa">
-                                </div>
+                                <span className="name-player1-fifa">Luca Guerrasio</span>
+                                <span className="role-player1-fifa">Responsabile formazione</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center fifa-card">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
+                            <div className="nick-player1-fifa">
+                                <span className="name-player1-fifa">Stefania Straniero</span>
+                                <span className="role-player1-fifa">Psicologa</span>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
-                            <img className="staff2" src="img/omino_stilizzato.png" alt="player-2" width="275" height="275"></img>
                             <div className="nick-player1-fifa">
-                                <span className="name-player1-fifa">Nutritionist</span>
-                                <span className="role-player1-fifa">Nutritionist</span>
-                                <div className="socials-player1-fifa">
-                                    
-                                </div>
+                                <span className="name-player1-fifa">Giacomo Astrua</span>
+                                <span className="role-player1-fifa">Nutrizionista</span>
+                            </div>
+                        </div>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 card-logo">
+                            <div className="nick-player1-fifa">
+                                <span className="name-player1-fifa">Floridiana Ventrella</span>
+                                <span className="role-player1-fifa">Consulente Post-Carriera</span>
                             </div>
                         </div>
                     </div>
